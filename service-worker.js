e7941014e0c8c5e11a3e2b16cd4749afa63a7b96
Adrián García Sabaté <akrian231@gmail.com>
2018-07-15 16:05:11 +0200

    201807151605

--- service-worker.js
+++ service-worker.js
@@ -9,17 +9,17 @@ if (workbox) {
 var dataCacheName = 'webprogapp';
 var cacheName = 'webprogappcache';
 var filesToCache = [
-  '/',
-  '/index.html',
-  '/basic.html',
-  '/scripts/main.js',
-  '/scripts/ws/runtime-caching.js',
-  '/styles/main.css',
-  '/images/hamburger.svg',
-  '/images/touch/apple-touch-icon.png',
-  '/images/touch/chrome-touch-icon-192x192.png',
-  '/images/touch/icon-128x128.png',
-  '/images/touch/ms-touch-icon-144x144-precomposed.png',
+  '',
+  'index.html',
+  'basic.html',
+  'scripts/main.js',
+  'scripts/ws/runtime-caching.js',
+  'styles/main.css',
+  'images/hamburger.svg',
+  'images/touch/apple-touch-icon.png',
+  'images/touch/chrome-touch-icon-192x192.png',
+  'images/touch/icon-128x128.png',
+  'images/touch/ms-touch-icon-144x144-precomposed.png',
 ];
 
 self.addEventListener('install', function(e) {
