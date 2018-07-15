e00c75b5e55597eea3a1eae6bd91c302fbc2fe99
Adrián García Sabaté <akrian231@gmail.com>
2018-07-15 12:42:27 +0200

    init

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+var dataCacheName = 'wpa';
+var cacheName = 'wpa';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/main.js',
+  '/scripts/ws/runtime-caching.js',
+  '/styles/main.css',
+  '/images/hamburger.svg',
+  '/images/touch/apple-touch-icon.png',
+  '/images/touch/chrome-touch-icon-192x192.png',
+  '/images/touch/icon-128x128.png',
+  '/images/touch/ms-touch-icon-144x144-precomposed.png',
+];
