1e81b1b89369d0a7c845a2e35a4d98647002bc47
Adrián García Sabaté <akrian231@gmail.com>
2018-07-15 15:59:50 +0200

    201807151559

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,11 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
 var dataCacheName = 'webprogapp';
 var cacheName = 'webprogappcache';
 var filesToCache = [
